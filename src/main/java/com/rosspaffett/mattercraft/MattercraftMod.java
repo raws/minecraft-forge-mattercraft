c61c84e6fa809d0d308f0bedee196b84c6945bf7
Ross Paffett <ross@rosspaffett.com>
2021-01-11 22:39:13 -0800

    Register as server-only mod

--- src/main/java/com/rosspaffett/mattercraft/MattercraftMod.java
+++ src/main/java/com/rosspaffett/mattercraft/MattercraftMod.java
@@ -4,9 +4,15 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.ServerChatEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.event.server.FMLServerStoppingEvent;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
+import net.minecraftforge.forgespi.language.IModInfo;
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -19,6 +25,7 @@ public class MattercraftMod {
     private ChatMessageSender outgoingMessageSender;
 
     public MattercraftMod() {
+        registerAsServerOnlyMod();
         MinecraftForge.EVENT_BUS.register(this);
     }
 
@@ -35,6 +42,21 @@ public class MattercraftMod {
     @SubscribeEvent
     public void onServerStopping(FMLServerStoppingEvent event) {
         stopSendingMessages();
+    }
+
+    /**
+     * Tell Forge to ignore any Mattercraft version mismatch between the client and server, since the server
+     * implementation is the only one that's used.
+     *
+     * This overrides the default display test extension point implemented in
+     * {@link net.minecraftforge.fml.ModContainer#ModContainer(IModInfo)} according to the example provided by
+     * {@link net.minecraftforge.fml.ExtensionPoint#DISPLAYTEST}.
+     */
+    private void registerAsServerOnlyMod() {
+        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(
+            () -> FMLNetworkConstants.IGNORESERVERONLY,
+            (remoteVersion, isNetwork) -> true
+        ));
     }
 
     private void sendChatMessage(String username, String body) {
