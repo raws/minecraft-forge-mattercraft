1eaecfd45b3c4900708d623444a52d9dc6cadbfd
Ross Paffett <ross@rosspaffett.com>
2021-01-17 16:21:46 -0800

    Don't prefix incoming messages with protocol name
    Matterbridge's API doesn't always include the chat protocol in
    its streaming JSON. The protocol doesn't seem like very
    useful information to display anyway.

--- src/main/java/com/rosspaffett/mattercraft/ChatMessage.java
+++ src/main/java/com/rosspaffett/mattercraft/ChatMessage.java
@@ -3,7 +3,6 @@ package com.rosspaffett.mattercraft;
 import net.minecraft.util.text.StringTextComponent;
 
 public class ChatMessage {
-    private String protocol;
     private final String text;
     private final String username;
 
@@ -20,28 +19,8 @@ public class ChatMessage {
         return this.username;
     }
 
-    public String humanizedProtocol() {
-        return MatterbridgeProtocol.humanize(protocol);
-    }
-
-    public String textWithUsername() {
-        return "<" + getUsername() + "> " + getText();
-    }
-
     public String toString() {
-        if (protocol == null) {
-            return toStringWithoutProtocol();
-        } else {
-            return toStringWithProtocol();
-        }
-    }
-
-    private String toStringWithProtocol() {
-        return "[" + humanizedProtocol() + "] " + textWithUsername();
-    }
-
-    private String toStringWithoutProtocol() {
-        return textWithUsername();
+        return "<" + getUsername() + "> " + getText();
     }
 
     public StringTextComponent toTextComponent() {
